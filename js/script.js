e8f6240a70f7ff0b7238c7a1a5839efe90193638
travis-mcginley <travis.o.mcginley@gmail.com>
2022-09-06 09:00:34 -0400

    Create 16x16 grid using script and add styling

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Create squares for a 16x16 grid
+for (let i = 0; i < (16*16); i++) {
+  let grid = document.querySelector(".grid");
+  let square = document.createElement("div");
+  square.classList.add("square");
+  grid.appendChild(square);
+}
