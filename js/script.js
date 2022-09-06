3cc1889df7c5337adfdf2a32bb879c9c210dc5a1
travis-mcginley <travis.o.mcginley@gmail.com>
2022-09-06 15:59:01 -0400

    Add option to clear sketch grid

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 // Create squares for a 16x16 grid
-for (let i = 0; i < (5*5); i++) {
-  let grid = document.querySelector(".grid");
-  let square = document.createElement("div");
-  square.classList.add("square");
-  grid.appendChild(square);
+
+let createGrid = (size) => {
+  for (let i = 0; i < (size*size); i++) {
+    let grid = document.querySelector(".grid");
+    let square = document.createElement("div");
+    square.classList.add("square");
+    grid.appendChild(square);
+  }
 }
+createGrid(64);
+
+// Show slider value above slider
+let sliderUpate = function(e) {
+  let sliderDisplay = document.querySelector(".display");
+  sliderDisplay.textContent = `${this.value} x ${this.value}`;
+  createGrid(parseInt(this.value));
+}
+let slider = document.querySelector("#slider");
+slider.addEventListener("change", sliderUpate);
+
+// Clear grid upon command
+let clearGrid = function(e) {
+  let grid = document.querySelectorAll(".grid > div");
+  grid.forEach(cell => cell.style.backgroundColor = "white");
+}
+let clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", clearGrid);
+
 
