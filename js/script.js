cea4a3e50508e7d3559efa105be03132eee8122b
travis-mcginley <travis.o.mcginley@gmail.com>
2022-09-06 10:48:42 -0400

    Remove id element for each square due to built in css function 'nth-child'

--- js/script.js
+++ js/script.js
@@ -3,6 +3,5 @@ for (let i = 0; i < (16*16); i++) {
   let grid = document.querySelector(".grid");
   let square = document.createElement("div");
   square.classList.add("square");
-  square.setAttribute("id", `square-${i + 1}`); // Give id to each square for individual modifications
   grid.appendChild(square);
 }
