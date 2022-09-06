8d8f0e1cc7bad579c53889bd907f6c9f0912286d
travis-mcginley <travis.o.mcginley@gmail.com>
2022-09-06 12:16:44 -0400

    Add controls, header, and footer to ui

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // Create squares for a 16x16 grid
-for (let i = 0; i < (16*16); i++) {
+for (let i = 0; i < (5*5); i++) {
   let grid = document.querySelector(".grid");
   let square = document.createElement("div");
   square.classList.add("square");
   grid.appendChild(square);
 }
+
